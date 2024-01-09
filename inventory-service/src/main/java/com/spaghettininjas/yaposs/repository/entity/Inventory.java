cb79c01bc3051e6d292309b2f2203497ba2d20e4
NGTV <67955457+Ne-Ga-TIV@users.noreply.github.com>
2024-01-09 19:20:14 +0200

    Update Inventory.java

--- inventory-service/src/main/java/com/spaghettininjas/yaposs/repository/entity/Inventory.java
+++ inventory-service/src/main/java/com/spaghettininjas/yaposs/repository/entity/Inventory.java
@@ -20,7 +20,7 @@ public class Inventory {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne()
+    @OneToOne()
     @JoinColumn(name = "productId")
     private Product product;
 
